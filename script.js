c0183f4c89d0b912ff65a6f9fdde449bdb21d364
thouseef-hamza <thouseefhamza.codes@gmail.com>
2024-06-25 14:34:21 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// ************************************************* //
+// ****************** Day 1 ************************ //
+// console.log("Hello world!!");
+
+// ************************************************* //
+// ****************** Day 2 ************************ //
+// console.log(__filename); // C:\Users\thous\OneDrive\Desktop\Node Learning\script.js
+// console.log(__dirname); // C:\Users\thous\OneDrive\Desktop\Node Learning git init
+
